85a7521abbe767fee54e71ff981f84bd9e7b5164
Arsim Kosumi <arsim.kosumi@ndbit.net>
2023-09-09 01:19:31 +0200

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,13 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://flakakallaba:flaka12345@cluster0.hyen6ow.mongodb.net/?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect('mongodb+srv://flakakallaba:flaka12345@cluster0.hyen6ow.mongodb.net/?retryWrites=true&w=majority', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${(conn).Connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${(conn).Connection.host}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
     process.exit(1)
