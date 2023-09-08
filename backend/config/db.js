7118025a47cf0cf00b6296fac7e560fcf1164533
Arsim Kosumi <arsim.kosumi@ndbit.net>
2023-09-09 01:15:56 +0200

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb://flakakallaba:flaka12345@cluster0.hyen6ow.mongodb.net/?retryWrites=true&w=majority', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${(conn).Connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold)
+    console.error(`Error: ${error.message}`)
     process.exit(1)
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -86,11 +85,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
